65e6533182da278cc92fdb5265841f8576f72d5f
Tony <tony@raisingthefloor.org>
2014-06-05 17:45:34 +0200

    Fixed bogus comment syntax

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var _ = require('underscore-node');
 // approximates our current approach of chaining defaults based on environment, simplified and hard-coded for this demo
 var defaultConfig = _.defaults(require('./configs/development.json'),require('./configs/defaults.json'));
 
-# In two lines, we can allow the user to override even deep hierarchical settings from the command line
+// In two lines, we can allow the user to override even deep hierarchical settings from the command line
 nconf.argv().defaults({"config": defaultConfig });
 var config = nconf.get("config");
 
